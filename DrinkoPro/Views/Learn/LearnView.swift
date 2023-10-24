130c83f5bb0cda50d1e2a482cbfa5408a8846b05
cilippofilia <filippocarlocilia@gmail.com>
2023-10-25 00:06:04 +0100

    added 4 books in all 4 languages. moving on to translate it to spanish

--- DrinkoPro/Views/Learn/LearnView.swift
+++ DrinkoPro/Views/Learn/LearnView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct LearnView: View {
     @Environment(\.horizontalSizeClass) var sizeClass
-    @StateObject var proLessons = ProLessonsViewModel()
+
     static let learnTag: String? = "Learn"
 
     let subjects = Bundle.main.decode([Subject].self, from: "lessons.json")
@@ -26,13 +26,6 @@ struct LearnView: View {
                         }
                     }
                 }
-
-                // TODO: UNCOMMENT WHEN DRINKO PRO IS READY
-//                Section(header: Text("Drinko Pro")) {
-//                    ForEach(proLessons.lessons) { lesson in
-//                        ProLessonRowView(proLesson: lesson)
-//                    }
-//                }
 
                 Section(header: Text("Superjuice calculators")) {
                     SuperjuiceRowView(juiceType: "Lime")
@@ -52,12 +45,6 @@ struct LearnView: View {
                 }
             }
             .navigationTitle("Learn")
-            .onAppear {
-                guard let url = URL(string: "https://raw.githubusercontent.com/cilippofilia/drinko-jsons/main/test.json") else {
-                    return
-                }
-                proLessons.fetchData(from: url)
-            }
         }
         .navigationViewStyle(StackNavigationViewStyle())
     }
