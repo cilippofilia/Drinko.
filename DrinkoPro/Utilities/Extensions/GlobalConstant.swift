6dfe35b722e4667b5efafb19d62a66429bcd7aba
Filippo Cilia <filippocarlocilia@gmail.com>
2024-03-17 17:56:17 +0000

    updated global constants

--- DrinkoPro/Utilities/Extensions/GlobalConstant.swift
+++ DrinkoPro/Utilities/Extensions/GlobalConstant.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 let deviceWidth = UIScreen.main.bounds.size.width /// Device width size
-let compactScreenWidth = deviceWidth / 1.1 /// iPhone padded view
-let regularScreenWidth = deviceWidth / 1.3 /// iPad padded view
+let compactScreenWidth = deviceWidth * 0.9 /// iPhone padded view
+let regularScreenWidth = deviceWidth * 0.7 /// iPad padded view
 
 // URLs inside the app
 let twitterURL = URL(string: "https://x.com/the_drinko_app")
 let instaURL = URL(string: "https://www.instagram.com/drinko_app")
 
-let drinkoURL = URL(string: "https://apps.apple.com/gb/app/drinko-cocktail-recipes-app/id6449893371")
-let rateURL = URL(string: "itms-apps://apps.apple.com/gb/app/drinko-cocktail-recipes-app/id6449893371?action=write-review")
+let drinkoURL = URL(string: "https://apps.apple.com/gb/app/drinko/id6449893371")
+let rateURL = URL(string: "itms-apps://apps.apple.com/gb/app/drinko/id6449893371?action=write-review")
