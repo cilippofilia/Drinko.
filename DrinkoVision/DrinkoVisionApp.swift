182eb3867b933fb1e81dbf3b5fd46c0d35d5bf66
Filippo Cilia <filippocarlocilia@gmail.com>
2024-06-24 14:42:08 +0100

    added visionOS logo

---
+++ DrinkoVision/DrinkoVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkoVisionApp.swift
+//  DrinkoVision
+//
+//  Created by Filippo Cilia on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct DrinkoVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
