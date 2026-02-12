32f11064f7b1fc8809af3ef5e758c3cda445b62d
Filippo Cilia <filippocarlocilia@gmail.com>
2026-02-12 18:55:20 +0000

    fix delete button on mac, remove sample data, fix cloudkit

--- DrinkoPro/DrinkoProApp.swift
+++ DrinkoPro/DrinkoProApp.swift
@@ -14,9 +14,21 @@ struct DrinkoProApp: App {
     @State private var favorites = Favorites()
     @State private var cocktailsViewModel = CocktailsViewModel()
     @State private var lessonsViewModel = LessonsViewModel()
+    @Environment(\.scenePhase) private var scenePhase
+    private let modelContainer: ModelContainer
 
     init() {
         try? Tips.configure()
+        do {
+            let config = ModelConfiguration(cloudKitDatabase: .automatic)
+            modelContainer = try ModelContainer(
+                for: Category.self,
+                Item.self,
+                configurations: config
+            )
+        } catch {
+            fatalError("Failed to create ModelContainer: \(error)")
+        }
     }
     
     var body: some Scene {
@@ -26,6 +38,6 @@ struct DrinkoProApp: App {
         .environment(favorites)
         .environment(cocktailsViewModel)
         .environment(lessonsViewModel)
-        .modelContainer(for: Category.self)
+        .modelContainer(modelContainer)
     }
 }
